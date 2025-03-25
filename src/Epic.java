0e504a1de1b9a4f01be23477fedeeccc5a13dc14
Lillisipp <lillisip@ya.ru>
2025-03-25 22:22:45 +0300

    Add files via upload

---
+++ src/Epic.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Epic extends Task {
+    private List<Integer> subtaskIds;
+
+    public Epic(String nameTask, String description) {
+        super(nameTask, description);
+        this.subtaskIds = new ArrayList<>();
+    }
+
+    public void addSubtask(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "id=" + getId() +
+                ", title='" + getNameTask() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status=" + getStatus() +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
+
+    public void updateStatus(HashMap<Integer, Subtask> subtasks) {
+        if (subtaskIds.isEmpty()) {
+            setStatus(Status.NEW);
+            return;
+        }
+        boolean subtaskDone = true;
+        boolean subtaskProces = false;// в процесе выполнения
+        for (Integer id : subtaskIds) {
+            Subtask subtask = subtasks.get(id);
+            if (subtask != null) {
+                if (subtask.getStatus() == Status.NEW) {
+                    subtaskDone = false;
+                } else if (subtask.getStatus() == Status.IN_PROGRESS) {
+                    subtaskDone = false;
+                    subtaskProces = true;
+                }
+            }
+        }
+        if (subtaskDone){
+            setStatus(Status.DONE);
+        } else if (subtaskProces) {
+            setStatus(Status.IN_PROGRESS);
+        } else  {
+            setStatus(Status.NEW);
+        }
+
+    }
+
+
+}
