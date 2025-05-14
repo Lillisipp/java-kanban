612e2773a6ba58581dddfec17cafbdcdb1e306d5
Lillisipp <Lillisip@ya.ru>
2025-05-14 23:25:47 +0300

    исправления и добавление пакетов

--- test/ru/yandex/task/manager/managers/FileBackedTaskManagerTest.java
+++ test/ru/yandex/task/manager/managers/FileBackedTaskManagerTest.java
@@ -1,20 +1,16 @@
 package ru.yandex.task.manager.managers;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-import ru.yandex.task.manager.exception.ManagerSaveException;
 import ru.yandex.task.manager.managers.impl.FileBackedTaskManager;
 import ru.yandex.task.manager.model.Epic;
 import ru.yandex.task.manager.model.Subtask;
 import ru.yandex.task.manager.model.Task;
 import ru.yandex.task.manager.model.enums.TaskType;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FileBackedTaskManagerTest {
 
@@ -53,9 +49,4 @@ public class FileBackedTaskManagerTest {
         Assertions.assertTrue(manager.getEpics().isEmpty());
         Assertions.assertTrue(manager.getSubtasks().isEmpty());
     }
-
-
-
-
 }
-
